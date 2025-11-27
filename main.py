71337cef67de7643fa2f85944ced40c371524060
Bardia <bardiagsz@gmail.com>
2025-11-27 18:45:12 +0100

    main file created

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():   
+    gerichtet=False
+
+    print("a)die anzahl von knoten")
+    print("b)die Namen von knoten")
+    print("c)die koordinaten von knoten")
+    eingabe=input("choose\n")
+
+    match eingabe:
+        case "a":
+            pass
+        case "b":
+            pass
+        case "c":
+            pass
+        case _:
+            print("ridi")
+
+if __name__ == "__main__":
+    main()
